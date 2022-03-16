a228aa632e7679bea2b1e8f9b60ff38637c2eab8
The-Astronautt <73796349+The-Astronautt@users.noreply.github.com>
2022-03-16 19:43:17 +0100

    2-printalpabets10times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 
+ * prints all the alphabets in lower case 10 times
+ */
+
+void print_alphabet_x10(void)
+{
+	int alp;
+	int br;
+
+	for (br = 0; br < 10; br++)
+	{
+		for (alp = 97; alp <= 122; alp++)
+		{
+			_putchar(alp);
+		}
+		_putchar('\n');
+	}
+}
