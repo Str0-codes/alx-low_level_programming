152f750fc9ca592b11b8e77f11b75fda34b20a2b
The-Astronautt <73796349+The-Astronautt@users.noreply.github.com>
2022-03-16 19:34:32 +0100

    1-alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,22 +1,22 @@
 #include "main.h"
 
 /**
- 
  * Print_alphabet -Print alphabet
- 
- * 
- 
+
+*
+
  */
 
 void print_alphabet(void)
 {
-	int alp;
+        int alp;
 
-	for (alp = 97; alp <= 122; alp++)
-	{
-		_putchar(alp);
+        for (alp = 97; alp <= 122; alp++)
+        {
+                _putchar(alp);
 
-	}
-	_putchar('\n');
+        }
+        _putchar('\n');
 }
 
+~
