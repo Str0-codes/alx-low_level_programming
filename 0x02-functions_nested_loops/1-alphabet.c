da603be1ebab7e41e236f780a629296d78463e1e
The-Astronautt <73796349+The-Astronautt@users.noreply.github.com>
2022-03-16 19:29:20 +0100

    1-alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * Prints small alphabet letters
+ * Returns the value of 0
+ */
+
+void print_alphabet(void)
+{
+	int alp;
+
+	for (alp = 97; alp <= 122; alp++)
+	{
+		_putchar(alp);
+
+	}
+	_putchar('\n');
+}
+
