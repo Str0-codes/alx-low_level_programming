40959e2235cd681a66a79424a33884b566fca5ce
The-Astronautt <73796349+The-Astronautt@users.noreply.github.com>
2022-03-16 19:48:24 +0100

    Lower or uppercase

---
+++ 0x02-functions_nested_loops/3-slower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - Determine if an alphabet is lowercase or upper case
+ *
+ * Return: return 1 if lowercase, returns 0 otherwise
+ *
+ */
+int _islower(int c)
+{
+
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
