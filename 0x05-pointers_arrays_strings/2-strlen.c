2db12b977a48c358c16523034af2ee3b981d5ba3
The-Astronautt <danielMMomodu@gmail.com>
2022-03-22 20:57:54 +0100

    Length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - output the lenght on a string
+ * @s: the string to be checked
+ * Return: Always return c
+ */
+
+int _strlen(char *s)
+{
+	 // '\0' represents end of String
+    for(i=0; str[i]!='\0'; ++i);
+       printf("\nLength of input string: %d",i);
+
+    return 0;
+}
